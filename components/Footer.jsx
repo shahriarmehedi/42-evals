c45de80d249be7b13c49bae240248843663424b2
shahriarmehedi <67407302+shahriarmehedi@users.noreply.github.com>
2024-08-09 03:06:32 +0600

    stable frontend 2

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,22 +2,11 @@ import React from 'react'
 
 function Footer() {
     return (
-        <footer className='bg-gray-800 text-white py-20'>
-            <div className="max-w-7xl mx-auto">
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                    <div>
-                        <h2 className="text-2xl font-bold">About Us</h2>
-                        <p className="mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget magna.</p>
-                    </div>
-                    <div>
-                        <h2 className="text-2xl font-bold">Contact Us</h2>
-                        <p className="mt-4">123 Main Street</p>
-                        <p>City, State</p>
-                        <p>
-                            <a href="tel:555-555-5555">555-555-5555</a>
-                        </p>
-                    </div>
-                </div>
+        <footer className='bg-gray-800 text-white py-14'>
+            <div className="max-w-7xl mx-auto text-gray-400">
+
+                <p className='text-center text-sm'>© 2024 42evals. All rights reserved.</p>
+
             </div>
         </footer>
     )
